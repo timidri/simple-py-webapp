e16eecff4a1afc08633246bc19686bb81ba0ae44
Walter Gildersleeve <walter@gildersleeve.eu>
2018-01-17 14:47:21 +0100

    first basic webpage

--- wsgi.py
+++ wsgi.py
@@ -5,19 +5,43 @@ def application(environ, start_response):
     txt = get_text()
     return [txt]
 
-def get_text():
-    tmp = """
-This is a template --- ${ write("pang") }$ -- That was a template
-"""
-    t = Templite(tmp)
-    rtn = t.render()
-    return rtn
+def get_header():
+    t = Templite(header_template)
+    title = "Default Page Title"
+    return t.render(title=title)
+
+def get_body():
+    t = Templite(body_template)
+    return t.render()
+
+def construct_page():
+    header = get_header()
+    body = get_body()
+    t = Templite(page_template)
+    return t.render(header=header, body=body)
 
 def application(environ, start_response):
     start_response('200 OK', [('Content-Type', 'text/html')])
-    txt = get_text()
+    txt = construct_page()
     return [txt]
 
+header_template = """
+    <title> ${ write(title) }$ </title>
+"""
 
-print get_text()
-    
+body_template = """
+<p>This is a test</p>
+"""
+
+page_template = """
+<html>
+    <head>
+${write(header)}$
+    </head>
+    <body>
+${write(body)}$
+    </body>
+</html>
+"""
+
+print construct_page()
